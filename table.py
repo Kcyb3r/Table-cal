4714a104f603a402fc304b54aee8ffdbdcd8613a
Kcyb3r <167519885+Kcyb3r@users.noreply.github.com>
2024-04-22 17:29:57 +0530

    Add files via upload

---
+++ table.py
@@ -0,0 +1,13 @@
+print("|-----------------------|")
+print("|--TABLES-CALCULATOR ---|")
+print("|-----------------------|\n")
+
+x = 1
+
+y = int(input("enter number : "))
+
+while x <= 10:
+    print(y*x)
+    x += 1
+
+print("|---Finished---|")
